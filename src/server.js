ad403b760ac9be8ebcbafeed7ebf3b874f5af3b7
fajarmaftuhfadli <fajarmaftuhfadli@gmail.com>
2022-03-13 20:06:29 +0700

    feat(server): create a new Hapi server
    it creates a new Hapi server on http://localhost:8000

--- src/server.js
+++ src/server.js
@@ -1,1 +1,22 @@
-console.log("Hello world");
+import Hapi from "@hapi/hapi";
+
+/**
+ * It creates a new Hapi server, sets its port and host, and starts the server
+ */
+const init = async () => {
+  const server = Hapi.server({
+    port: 8000,
+    host: "localhost",
+  });
+
+  await server.start();
+  console.log("Server running on %s", server.info.uri);
+};
+
+/* A way to handle errors that are not handled by any other error handler. */
+process.on("unhandledRejection", (err) => {
+  console.log(err);
+  process.exit(1);
+});
+
+init();
